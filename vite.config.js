a96dbf5b8e7d44b2636659fa90ff7de19e4f07bd
Noah Hoff <noah.hoff@outlook.com>
2025-02-25 21:13:14 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
-  build: {
-    sourcemap: false, // disable source maps in production
-  },
-})
+  base: '/CVportal-React/',
+});
