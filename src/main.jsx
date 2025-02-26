00733909c43807704d3bd7ce0d4c5c676f1935af
NoahtlHoff <noah.hoff@outlook.com>
2025-02-26 11:51:05 +0100

    .

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter  basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
